6ef33a3a476950eef014aff8be3f808a2c97b37f
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-09-08 04:23:02 +0200

    Abort change on ft_strmap and ft_strmapi

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 00:33:10 by asoursou          #+#    #+#             */
-/*   Updated: 2019/09/08 04:08:29 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/09/08 04:21:05 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 char	*ft_strmap(char const *s, char (*f)(char))
 {
 	char	*d;
-	size_t	i;
+	size_t	n;
 
-	if ((d = (char*)malloc((ft_strlen(s) + 1) * sizeof(char))))
+	n = ft_strlen(s) + 1;
+	if ((d = (char*)malloc(n * sizeof(char))))
 	{
-		i = 0;
-		while (*s)
-			d[i++] = f(*s++);
-		d[i] = '\0';
+		d[--n] = '\0';
+		while (n--)
+			d[n] = f(s[n]);
 	}
 	return (d);
 }
