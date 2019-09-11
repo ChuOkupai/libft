488413c7548498e535ffe4cb0b0d506454288b6c
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-09-11 20:33:42 +0200

    simplified ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,10 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 03:07:21 by asoursou          #+#    #+#             */
-/*   Updated: 2019/08/15 05:51:28 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/09/11 17:02:06 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 static int	ft_iswspaces(int c)
@@ -20,20 +19,12 @@ static int	ft_iswspaces(int c)
 
 char		*ft_strtrim(char const *s)
 {
-	char	*d;
-	size_t	n;
+	size_t n;
 
 	while (ft_iswspaces(*s))
 		s++;
-	n = 0;
-	while (s[n])
-		n++;
+	n = ft_strlen(s);
 	while (n && ft_iswspaces(s[n - 1]))
 		n--;
-	if ((d = (char*)malloc((n + 1) * sizeof(char))))
-	{
-		ft_memcpy(d, s, n);
-		d[n] = '\0';
-	}
-	return (d);
+	return (ft_strsub(s, 0, n));
 }
