24d172db9c036d9c01c382b5ebb2fc97966c07e0
Adrien SOURSOU <asoursou@student.42.fr>
2019-11-11 17:20:56 +0100

    mv ft_list_find

--- src/list/ft_list_search.c
+++ src/list/ft_list_search.c
@@ -6,13 +6,13 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 00:40:08 by asoursou          #+#    #+#             */
-/*   Updated: 2019/11/10 18:32:37 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/11/11 15:59:33 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_list.h"
 
-t_list	*ft_list_find(t_list *l, const void *ref,
+t_list	*ft_list_search(t_list *l, const void *ref,
 		int (*cmp)(const void *ref, const void *content))
 {
 	while (l && cmp(ref, l->content))
