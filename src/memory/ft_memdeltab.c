c562259941db38543e15ee5c6b33c99815f67003
Adrien SOURSOU <asoursou@student.42.fr>
2019-12-03 11:48:28 +0100

    ft_strsplit fix

--- src/memory/ft_memdeltab.c
+++ src/memory/ft_memdeltab.c
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memdel.c                                        :+:      :+:    :+:   */
+/*   ft_memdeltab.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/12 02:47:22 by asoursou          #+#    #+#             */
-/*   Updated: 2019/12/03 11:44:42 by asoursou         ###   ########.fr       */
+/*   Created: 2019/12/03 11:07:31 by asoursou          #+#    #+#             */
+/*   Updated: 2019/12/03 11:42:40 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_memdel(void **p)
+void	*ft_memdeltab(void ***tab, size_t len)
 {
-	if (*p)
-	{
-		free(*p);
-		*p = NULL;
-	}
+	while (len)
+		free((*tab)[--len]);
+	free(*tab);
+	return (*tab = NULL);
 }
--- src/string/ft_strsplit.c
+++ src/string/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 03:29:19 by asoursou          #+#    #+#             */
-/*   Updated: 2019/11/28 23:36:17 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/12/03 11:39:03 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,19 @@ char			**ft_strsplit(char const *s, char c)
 
 	s2 = s;
 	i = 0;
-	while ((n = ft_wordlen(&s2, c)) && ++i)
+	while ((n = ft_wordlen(&s2, c)))
+	{
+		++i;
 		s2 += n;
-	if (!(t = (char**)malloc((n + 1) * sizeof(char*))))
+	}
+	if (!(t = (char**)malloc((i + 1) * sizeof(char*))))
 		return (NULL);
 	i = 0;
 	while ((n = ft_wordlen(&s, c)))
-	{
 		if (!(t[i++] = ft_strsub(s, 0, n)))
 			return (ft_memdeltab((void***)&t, i - 1));
-		s += n;
-	}
+		else
+			s += n;
 	t[i] = 0;
 	return (t);
 }
