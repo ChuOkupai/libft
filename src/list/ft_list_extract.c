86129b8353a5166226c1d9bbcf46ef7192834a19
Adrien SOURSOU <asoursou@student.42.fr>
2019-12-25 05:49:13 +0100

    Fix ft_list_extract

--- src/list/ft_list_extract.c
+++ src/list/ft_list_extract.c
@@ -6,21 +6,21 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 11:44:16 by asoursou          #+#    #+#             */
-/*   Updated: 2019/11/10 18:32:24 by asoursou         ###   ########.fr       */
+/*   Updated: 2019/12/25 05:46:21 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_list.h"
 
-t_list	*ft_list_extract(t_list **l, const void *ref,
-		int (*cmp)(const void *ref, const void *content))
+t_list	*ft_list_extract(t_list **l, const void *reference,
+		int (*cmp)(const void *reference, const void *content))
 {
 	t_list *e;
 	t_list *p;
 
 	e = *l;
 	p = NULL;
-	while (e && cmp(e->content, ref))
+	while (e && cmp(reference, e->content))
 	{
 		p = e;
 		e = e->next;
